5b19b15fa206e883d6d9737bcc9b4a0a514755b5
markus <markus.andersson97@gmail.com>
2021-11-24 18:27:28 +0200

    Added inital browse page. Fixed start script

--- frontend/src/Browse.tsx
+++ frontend/src/Browse.tsx
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 const Link = require('react-router-dom').Link;
 
+const bids = ['rockstar wig', 'kova peruukki', 'the malder', 'blondie']
+
 const Browse = () => {
 	return (
 	  <div className='browse'>
 		<h1 className='browse-header'>Mald.fi</h1>
 		<p>You got bald? Don't be mald, we got you!</p>
+		<h2>Current bids:</h2>
+		<ul>
+			{bids.map((bid) => (
+				<li>{bid}</li>
+			))}
+		</ul>
 	  </div>
 	)
 }
