2b666c9b19bb9f0fc5f4bfdd50ae429b3e7037af
r0bert1 <rpakkane@gmail.com>
2021-12-01 17:40:54 +0200

    Added broadcasting bids to other servers

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import axios from 'axios'
-// import config from './config.json'
+import config from './config.json'
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -10,7 +10,7 @@ import { Server } from 'socket.io'
 
 const itemRouter = require('./routers/item')
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
@@ -18,7 +18,6 @@ const io = new Server(server, {
     credentials: true,
   },
 })
-
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -28,34 +27,36 @@ app.get('/', (_req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/announcement', (_req, _res) => {
-  console.log('received')
+app.post('/bid', (req, _res) => {
+  console.log(req.body)
 })
 
 app.get('/ping', async (_req, res) => {
-  await axios
-    .post('http://localhost:3003/announcement', {
-      message: 'hello',
-    })
-    .then((res) => {
-      console.log(`statusCode: ${res.status}`)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-
   res.send('pong')
 })
 
-io.on('connection', (socket) => {
-	console.log('a user connected');
-	socket.on('bid', console.log);
-	socket.on('disconnect', () => {
-		console.log('user disconnected');
-	});
-});
-
 const PORT = process.env.PORT || 3001
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('bid', (data) => {
+    config.ports
+      .filter((port) => port !== PORT)
+      .forEach((port) => {
+        axios
+          .post(`http://localhost:${port}/bid`, data)
+          .then((res) => {
+            console.log(`statusCode: ${res.status}`)
+          })
+          .catch((error) => {
+            console.error(error)
+          })
+      })
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
