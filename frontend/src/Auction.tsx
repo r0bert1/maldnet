21e98dcb0f0ef29ff0c358843c3af3b55be5b6dd
r0bert1 <rpakkane@gmail.com>
2021-12-01 17:21:33 +0200

    Refactoring

--- frontend/src/Auction.tsx
+++ frontend/src/Auction.tsx
@@ -1,42 +1,49 @@
-import React, { Component } from 'react';
-import { Socket } from 'socket.io-client';
+import React, { Component } from 'react'
+import { Socket } from 'socket.io-client'
 
-const Link = require('react-router-dom').Link;
+const Link = require('react-router-dom').Link
 
-class Auction extends Component <{socket:Socket}, {bid:number, currentBid:number, itemId:number}>{
-  constructor(props:any) {
-    super(props);
+class Auction extends Component<
+  { socket: Socket },
+  { bid: number; currentBid: number; itemId: number }
+> {
+  constructor(props: any) {
+    super(props)
     this.state = {
-	  bid: 0,
-	  currentBid: 69,
-	  itemId: +(window.location.pathname.split("/").pop() ?? 0),
-    };
+      bid: 0,
+      currentBid: 69,
+      itemId: +(window.location.pathname.split('/').pop() ?? 0),
+    }
   }
 
   sendBid(socket: Socket) {
-	socket.emit("bid", {
-		userId: 123,
-		itemId: this.state.itemId,
-		bid: this.state.bid
-	});
+    socket.emit('bid', {
+      userId: 123,
+      itemId: this.state.itemId,
+      bid: this.state.bid,
+    })
   }
 
   render() {
     return (
-      <div className='browse'>
-        <h1 className='browse-header'>Wig item number {this.state.itemId}</h1>
+      <div className="browse">
+        <h1 className="browse-header">Wig item number {this.state.itemId}</h1>
         <h3>Current price: {this.state.currentBid}$</h3>
         <p>Item description</p>
         <div>
           <p>place your bid:</p>
-          <input onChange={(event) => this.setState({ bid: +event.target.value })} 
-            type="number" 
-            value={this.state.bid}></input>
-          <button onClick={() => this.sendBid(this.props.socket)}>Place bid</button>
-          </div>
+          <input
+            onChange={(event) => this.setState({ bid: +event.target.value })}
+            type="number"
+            value={this.state.bid}
+          ></input>
+          <button onClick={() => this.sendBid(this.props.socket)}>
+            Place bid
+          </button>
+        </div>
       </div>
     )
   }
 }
 
-export default Auction;
+export default Auction
