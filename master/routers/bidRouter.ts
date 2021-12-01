31a7ab8b0685d782903ec03def837e1effb25604
r0bert1 <rpakkane@gmail.com>
2021-12-01 17:02:54 +0200

    fixed implicit any

--- master/routers/bidRouter.ts
+++ master/routers/bidRouter.ts
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.post('/bid', (req,_res)=>{
+router.post('/bid', (req: any, _res: any) => {
   const bid = req.body.bid
   console.log(`bid received: ${bid}`)
 })
