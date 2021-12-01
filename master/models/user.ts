ad2dd0a75da30408d32d9aeb9705ff4d6b6bef5b
Arttu Janhunen <arttu.janhunen@gmail.com>
2021-12-01 14:57:25 +0200

    add user and item schemas

---
+++ master/models/user.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  pwd: {
+    type: String,
+    required: true
+  }
+
+})
+
+userSchema.set('toJSON', {
+  transform:(_document, retObj) => {
+    retObj.id = retObj._id.toString()
+    delete retObj._id
+    delete retObj.__v
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
